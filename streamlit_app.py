2053c3716534e198f1ca7f01e11b8b5ad78530d4
Padmasree R <79768290+padmasre@users.noreply.github.com>
2023-09-16 20:57:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write('Hi!')
 
 with st.sidebar:
   st_button('linkedin', 'https://www.linkedin.com/in/padmasreers', 'linkedin.com/in/padmasreers', 20)
+
+text_input = st.text_input(
+  "Enter your movie review below 👇",
+  "The movie was fun to watch with family",
+  key="userinput",
+)
+
+if text_input:
+    st.write("You wrote: ", text_input)
