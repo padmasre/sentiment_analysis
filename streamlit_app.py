85c93480f9adc094e835cc8791e4c546d5535a84
Padmasree R <79768290+padmasre@users.noreply.github.com>
2023-09-16 21:08:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from st_functions import st_button, load_css
 
-st.write('Hi!')
+
+with st.sidebar:
+  st_button('linkedin', 'https://www.linkedin.com/in/padmasreers', 'linkedin.com/in/padmasreers', 20)
 
 text_input = st.text_input(
   "Enter your movie review below 👇",
