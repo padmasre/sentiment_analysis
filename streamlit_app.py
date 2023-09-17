9ccd0bdd0d50f87a49583a1010ba18d9fbf088fd
Padmasree R <79768290+padmasre@users.noreply.github.com>
2023-09-17 13:11:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,15 @@ with st.sidebar:
   st_button('linkedin', 'https://www.linkedin.com/in/padmasreers', 'linkedin.com/in/padmasreers', 20)
   st_button('github', 'https://github.com/padmasre/sentiment_analysis', 'github.com/padmasre/sentiment_analysis', 20)
 
-text_input = st.text_area(
-  "Enter your movie review below 👇",
-  placeholder="The movie was fun to watch with family",
-  key="userinput",
+movie_name = st.text_input(
+  "Enter the name of the move here",
+  placeholder="Enter the movie name.",
+  key="moviename"
 )
 
-if text_input:
-    st.write("You wrote: ", text_input)
+if len(movie_name) > 0:
+  reviews = st.text_area(
+    "Enter movie reviews below 👇",
+    placeholder="Enter one review per line. Example:\n The movie was fun.\nIt was disappointing",
+    key="reviews",
+  )
