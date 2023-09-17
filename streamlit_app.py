d6361ca3ab251ae02fcdbbf75ee9179d972daf55
Padmasree R <79768290+padmasre@users.noreply.github.com>
2023-09-17 17:57:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if len(reviews) > 0:
   st.bar_chart(
     chart_data,
     y=['Positive', 'Negative'],
-    color=['#FF0000','#0000FF'] 
+    color=['#00b300','#0000FF'] 
   )
   # for i in review_predictions:
   #   if i == 1:
