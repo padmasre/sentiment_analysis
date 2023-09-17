861b7e64abd7cabe2773f092ffd67ba1d46452a7
Padmasree R <79768290+padmasre@users.noreply.github.com>
2023-09-16 20:48:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ from st_functions import st_button, load_css
 
 st.write('Hi!')
 
-st_button('linkedin', 'https://www.linkedin.com/in/padmasreers', 'LinkedIn Profile', 20)
+st_button('linkedin', 'https://www.linkedin.com/in/padmasreers/', 'LinkedIn Profile', 20)
